65caf1f70fbd4a358c335e1ce6b096151c89638a
fundspiAndroid <mudraangle@gmail.com>
2023-08-28 18:30:14 +0530

    jail break  commit - 28.08.2023

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:animerush/screens/bottomBar.dart';
 import 'package:animerush/screens/splash.dart';
 import 'package:animerush/utils/appRoutes.dart';
 import 'package:animerush/utils/appTheme.dart';
+import 'package:animerush/widgets/customButtons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
 
@@ -48,8 +50,63 @@ Future<void> mainCommon(FlavorConfig config) async {
 }
 */
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool? _jailbroken;
+  bool? _developerMode;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    final appTheme = Theme.of(context);
+
+    bool jailBroken;
+    bool developerMode;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      jailBroken = await FlutterJailbreakDetection.jailbroken;
+      developerMode = await FlutterJailbreakDetection.developerMode;
+
+      if (jailBroken) {
+        Get.defaultDialog(
+          title: "Device Security Warning",
+          middleText: "This app cannot be used on a jailbroken or rooted device.",
+          titleStyle: appTheme.textTheme.bodyLarge,
+          middleTextStyle: appTheme.textTheme.labelSmall,
+          cancel: textButton(text: 'Ok', onPressed: () {}),
+          radius: 6,
+          titlePadding: const EdgeInsets.only(top: 15),
+          contentPadding: const EdgeInsets.symmetric(vertical: 15),
+          backgroundColor: appTheme.colorScheme.background,
+          barrierDismissible: false,
+        );
+      }
+    } on PlatformException {
+      jailBroken = true;
+      developerMode = true;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _jailbroken = jailBroken;
+      _developerMode = developerMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
